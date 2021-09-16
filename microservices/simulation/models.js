cd8583ec1ebba14f5183a0c4f6b1f407d1adc38f
Don Kushan <dknathalage@gmail.com>
2021-09-16 21:09:04 +1000

    models.js

---
+++ microservices/simulation/models.js
@@ -0,0 +1,29 @@
+class Light {
+	id
+	uuid
+	data
+}
+
+class User {
+	email
+	pass
+}
+
+class DataInstance {
+	id
+	data
+	state
+}
+
+class ControlState {
+	controlstate
+}
+
+class HealthInstance {
+	id
+	state
+}
+
+class HealthState {
+	healthstate
+}

ef56200ca35d603c2a4a57d251e40e3c0caa42d9
Don Kushan <dknathalage@gmail.com>
2021-09-16 20:51:58 +1000

    arch-change

---
+++ microservices/data-service/readme.md
@@ -0,0 +1,10 @@
+# Data Service
+
+## Functions
+
+- Subscribe to the network ✅ <br>
+- Unsubscribe from a network ✅<br>
+- Light Verification 🟡<br>
+- Query data ✅<br>
+
+All the data are accessed from <code>/graphql</code> end point
--- microservices/data-service/src/index.ts
+++ microservices/data-service/src/index.ts
@@ -1,16 +1,58 @@
+import { ApolloServer } from "apollo-server-express";
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
 import express from "express";
-import os from "os"
-const port = process.env.PORT;
+import mongoose from "mongoose";
+import http from "http";
+import os from "os";
 
-var app = express();
+import typeDefs from "./typedefs";
+import resolvers from "./resolvers";
 
-// define a route handler for the default home page
-app.get( "/", ( req, res ) => {
-    // render the index template
-    res.send( "data-service active, host: " +  os.hostname());
-} );
+const port = process.env.PORT || 4000;
+const user = process.env.USER_SERVICE || "http://localhost:4000/";
 
-// start the express server
-app.listen(port, () => {
-    console.log( `server started at http://127.0.0.1:${ port }` );
-} );
+const app = express();
+
+app.use("/graphql", (req, res, next) => {
+  if (req.headers.token === undefined)
+    return res.send(400).send("no auth token");
+  http.request(
+    {
+      hostname: user,
+      method: "GET",
+      headers: {
+        token: req.headers.token,
+      },
+    },
+    (resp) => {
+      console.log(resp.statusMessage);
+    }
+  );
+  next();
+});
+
+async function startApolloServer(typeDefs: any, resolvers: any) {
+  await mongoose.connect(
+    "mongodb+srv://root:toor@sit314.g7vp8.mongodb.net/test?retryWrites=true&w=majority"
+  );
+
+  const httpServer = http.createServer(app);
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  await new Promise((resolve) => httpServer.listen({ port: port }, resolve));
+  console.log(
+    `server started at http://127.0.0.1:${port + server.graphqlPath}`
+  );
+}
+
+startApolloServer(typeDefs, resolvers);
+
+app.get("/", (req, res) => {
+  // render the index template
+  res.send("device-service active 💡, host: " + os.hostname());
+});
